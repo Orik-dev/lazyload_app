82e2faa69ad613c833830d50154a140428a34501
orxan <orxan4ik7777@gmail.com>
2024-02-21 17:54:23 +0400

    route

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  static final  mainNavigation = MainNavigation();
+  static final mainNavigation = MainNavigation();
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
