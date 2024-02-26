9adc4c996875f7b9f0b91eaff43987caf9fff4b8
orxan <orxan4ik7777@gmail.com>
2024-02-26 21:08:54 +0400

    img + rout

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,23 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String backfire = 'images/backfire.jpg';
+  static const String fastfuriosX = 'images/fastfuriosX.jpg';
+  static const String fireGamer = 'images/fireGamer.jpg';
+  static const String first = 'images/first.jpg';
+  static const String five = 'images/five.jpg';
+  static const String fore = 'images/fore.jpg';
+  static const String free = 'images/free.jpg';
+  static const String logo = 'images/logo.svg';
+  static const String moviePlacholder = 'images/movie_placholder.jpeg';
+  static const String napoleon = 'images/napoleon.jpg';
+  static const String six = 'images/six.jpg';
+  static const String soldat = 'images/soldat.jpg';
+  static const String sozdatel = 'images/sozdatel.jpg';
+  static const String trailerBackground = 'images/trailer_background.jpeg';
+  static const String trailerPreview = 'images/trailer_preview.jpeg';
+  static const String two = 'images/two.jpg';
+  static const String uravnitel = 'images/uravnitel.jpg';
+}
